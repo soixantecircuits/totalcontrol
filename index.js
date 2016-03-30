06ceee14b3e2f6f2a71384c270c1b3d5960eb8ae
gabrielstuff <gabriel@soixantecircuits.fr>
2016-03-31 00:03:31 +0200

    aggragating multiples functions

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+var exphbs = require('express-handlebars')
+var _ = require('lodash')
 
-server.listen(8080)
+var activeSocket = [
+  {
+    name: 'test',
+    commands: [
+      {
+        name: 'start-photoshop'
+      },
+      {
+        name: 'shutdown'
+      }
+    ]
+  }
+]
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(require('express').static('public'))
+
+app.get('/', function(req, res) {
+  res.render('home')
 })
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data)
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
+app.get('/command', function(req, res) {
+  res.render('command', {
+    sockets: activeSocket
   })
 })
+
+app.get('/exec/:name/:command', function(req, res) {
+  console.log('req.params.id: ', req.params.name)
+  console.log('req.params.command: ', req.params.command)
+  var remote = _.find(activeSocket, ['name', req.params.name])
+  if (remote) {
+    remote.socket.emit(req.params.command)
+  }
+  res.end()
+})
+
+io.on('connection', function(socket) {
+  socket.emit('news', { hello: 'world' })
+  socket.on('registerCommands', function(data) {
+    if (checkIntegretity(data)) {
+      data.socket = socket
+      updateActiveSocket(data)
+    }
+  })
+})
+
+function checkIntegretity(data) {
+  if (data.name !== '') {
+    return true
+  } else {
+    return false
+  }
+}
+
+function updateActiveSocket(data) {
+  activeSocket.push(data)
+}
+
+server.listen(8080)
